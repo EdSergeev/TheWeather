074117668c726e5a091436567b149e950e2e854f
EduardSergeev <regedik@gmail.com>
2025-06-12 10:07:20 +0100

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.example.theweather"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.theweather"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    // Core
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    // UI Bundle
+    implementation(libs.bundles.compose.ui)
+    implementation(platform(libs.androidx.compose.bom))
+
+    // Debug UI
+    debugImplementation(libs.bundles.compose.ui.debug)
+
+    // Testing
+    testImplementation(libs.bundles.unit.testing)
+    androidTestImplementation(libs.bundles.android.testing)
+}
